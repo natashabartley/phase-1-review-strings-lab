964b21d16bb7880a913fad5e150a3ab841f54ce7
Natasha Bartley <+>
2023-03-21 18:59:41 -0400

    completed javascript strings  lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+let currentUser = ("Natasha");
+
+const welcomeMessage = (`Welcome to Flatbook, ${currentUser} !`);
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const excitedWelcomeMessage = ('WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '!');
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+const shortGreeting = ('Welcome, ' + firstInitial + '!');
+
+
